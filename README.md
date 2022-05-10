fd81d9342434ed80cd93409c727da9723e935724
khantorot <khantorot@gmail.com>
2022-05-11 03:33:00 +0600

    init

---
+++ README.md
@@ -0,0 +1,42 @@
+<div align="center">
+  <a href="#top">
+    <img src="content/icons/shortcut.png" alt="logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">List</h3>
+
+  <p align="center">
+    List of all projects.
+    <br />
+    <br />
+    <a href="https://khantorot.github.io/list">View Project</a>
+    ·
+    <a href="https://github.com/khantorot/list/issues">Report Bug</a>
+    ·
+    <a href="#top">Explore the docs</a>
+  </p>
+</div>
+
+
+
+
+## About The Project
+
+* Thanks to Google Chrome for desing.
+
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](https://github.com/khantorot/list/blob/main/LICENSE) file for details
+
+
+
+
+## Contact
+
+Email - khantorot@gmail.com
+
+
+
+<p align="right"><a href="#top">back to top ↑</a></p>
---
+++ assets/js/data.js
@@ -0,0 +1,284 @@
+const data = [
+    {
+        group: 'profile',
+        symbol: '🎭',
+        work: [
+            {
+                title: 'view',
+                symbol: '👣',
+                link: 'https://khantorot.github.io/view',
+                description: 'Portfolio. Content was taken from an unknown resource.'
+            },
+            {
+                title: 'room',
+                symbol: '👽',
+                link: 'https://khantorot.github.io/room',
+                description: 'Fictional room. Content was taken from an unknown resource.'
+            },
+            {
+                title: '2020',
+                symbol: '🐠',
+                link: 'https://khantorot.github.io/2020',
+                description: '2020 selected works. Temporary content - Runaway.'
+            },
+            {
+                title: 'cv',
+                symbol: '📂',
+                link: 'https://khantorot.github.io/cv',
+                description: 'Curriculum vitae.'
+            }
+        ]
+    },
+    {
+        group: 'craft',
+        symbol: '🐵',
+        work: [
+            {
+                title: 'sound',
+                symbol: '📀',
+                link: 'https://khantorot.github.io/sound',
+                description: 'Authors album. Temporary content - Kanye West.'
+            },
+            {
+                title: 'scene',
+                symbol: '🎬',
+                link: 'https://khantorot.github.io/scene',
+                description: 'Authors films. Inspired by BB. Temporary content - Quentin Tarantino.'
+            },
+            {
+                title: 'move',
+                symbol: '🎮',
+                link: 'https://khantorot.github.io/move',
+                description: 'Gamedev studio. Temporary content- Steam.'
+            },
+            {
+                title: 'bloo',
+                symbol: '💤',
+                link: 'https://khantorot.github.io/bloo',
+                description: 'Garage project together with Wama. Content was taken from an unknown resource.'
+            },
+        ]
+    },
+    {
+        group: 'case',
+        symbol: '⏰',
+        work: [
+            {
+                title: 'archive',
+                symbol: '💾',
+                link: 'https://khantorot.github.io/archive',
+                description: 'List of archival projects. Content was taken from an unknown resource.'
+            },
+            {
+                title: 'lab',
+                symbol: '👺',
+                link: 'https://khantorot.github.io/lab',
+                description: 'List of the lab projects. Content was taken from an unknown resource.'
+            },
+            {
+                title: 'intra42',
+                symbol: '🎓',
+                link: 'https://khantorot.github.io/intra42',
+                description: 'List of intra42 projects. Great gratitudes to school 21, adm, vkuswill.'
+            },
+            {
+                title: 'moscowtravelhack',
+                symbol: '💯',
+                link: 'https://khantorot.github.io/moscowtravelhack',
+                description: 'Moscow Travel Hack 2020. Great gratitudes for team and adm. Content was taken from an unknown resource.'
+            }
+        ]
+    },
+    {
+        group: 'factory',
+        symbol: '🌍',
+        work: [
+            {
+                title: 'gazon',
+                symbol: '🌵',
+                link: 'https://khantorot.github.io/gazon',
+                description: 'Fictional online store. Temporary content was taken from an unknown resource.'
+            },
+            {
+                title: 'empirebay',
+                symbol: '🔨',
+                link: 'https://khantorot.github.io/empirebay',
+                description: 'Fictional hotel. Temporary content was taken from an unknown resource.'
+            },
+            {
+                title: 'myimyi',
+                symbol: '😻',
+                link: 'https://khantorot.github.io/myimyi',
+                description: 'Fictional coffee bar. Special thanks to Hilltop coffee shop for coffee. Temporary content was taken from an unknown resource.'
+            },
+            {
+                title: 'hansabik',
+                symbol: '🧦',
+                link: 'https://khantorot.github.io/hansabik',
+                description: 'Fictional Charitable Foundation. Temporary content was taken from an unknown resource.'
+            },
+        ]
+    },
+    {
+        group: 'kitchen',
+        symbol: '🦋',
+        work: [
+            {
+                title: 'folio',
+                symbol: '🌳',
+                link: 'https://khantorot.github.io/folio',
+                description: 'The first portfolio of fictional projects. There are no updates.',
+            },
+            {
+                title: 'xyz',
+                symbol: '🛴',
+                link: 'https://khantorot.github.io/xyz',
+                description: 'Xyz is the project from the first portfolio. It is a Fictional company by developing AI. Special thanks to J. Aalambek (Luchy) for the video edition. The content was taken from an unknown resource. There are no updates.'
+            },
+            {
+                title: 'atom',
+                symbol: '🌞',
+                link: 'https://khantorot.github.io/atom',
+                description: 'Atom is the project from the first portfolio. Fictional hackathon. The content was taken from an unknown resource. There are no updates.'
+            },
+            {
+                title: 'kismod',
+                symbol: '🌸',
+                link: 'https://khantorot.github.io/kismod',
+                description: 'Kismod is the project from the first portfolio. Fictional web studio. Great gratitude for photo to A. Janybek (Jony). The other content was taken from an unknown resource. There are no updates.'
+            },
+        ]
+    },
+    {
+        group: 'commerce',
+        symbol: '🤖',
+        work: [
+            {
+                title: 'escrow',
+                symbol: '🤑',
+                link: 'https://khantorot.github.io/escrow',
+                description: 'This website is aimed to foreign citizens for a financial company. Special appreciation for Akkens support. Content was taken from an unknown resource.'
+            },
+            {
+                title: 'pizpizza',
+                symbol: '🍕',
+                link: 'https://khantorot.github.io/pizpizza',
+                description: 'This site is a business card for selling online food stores with delivery during the pandemic COVID2020. Great gratitude to Shamen (Shama) for card design. Temporary content was taken from an unknown resource.'
+            },
+            {
+                title: 'iq',
+                symbol: '💩',
+                link: 'https://khantorot.github.io/iq',
+                description: 'Web studio located in Bishkek. Big gratitudes to Dastan for design and support. Special thanks for design of the modal window to Shamen (Wama)'
+            },
+            {
+                title: 'webhost',
+                symbol: '🤡',
+                link: 'https://khantorot.github.io/webhost',
+                description: 'Hosting company in Bishkek.'
+            },
+            {
+                title: 'lucktime',
+                symbol: '🌭',
+                link: 'https://khantorot.github.io/lucktime',
+                description: 'Unfinished project. Online lottery. The customer did not receive the relevant documents to continue the project. Special thanks for Myrza (Vanya). Temporary content was taken from an unknown resource.'
+            },
+            {
+                title: 'innovation',
+                symbol: '🎈',
+                link: 'https://khantorot.github.io/innovation',
+                description: 'Unfinished project. A platform for citizens which created to unite efforts. The purpose of the project was not prompt. The tasks were not completed. Temporary content was taken from an unknown resource.'
+            },
+            {
+                title: 'ecommerce',
+                symbol: '🧀',
+                link: 'https://github.com/khantorot/ecommerce',
+                description: 'React and Node fully-functional e-commerce website. Ecommerce website using Vanilla JavaScript and Node.JS, ExpressJS and MongoDB.'
+            },
+            {
+                title: 'strapi',
+                symbol: '🥩',
+                link: 'https://github.com/khantorot/strapi',
+                description: 'Web application template. Strapi.'
+            },
+        ]
+    },
+    {
+        group: 'school21',
+        symbol: '🍄',
+        work: [
+            {
+                title: 'piscine_reloaded',
+                symbol: '🍍',
+                link: 'https://github.com/khantorot/piscine_reloaded',
+                description: 'Review the basics of the piscine with a series of exercises inspired from it.'
+            },
+            {
+                title: 'libft',
+                symbol: '🍉',
+                link: 'https://github.com/khantorot/libft',
+                description: 'This first project as a 42 year old student will help you consolidate your swimming pool skills.'
+            },
+            {
+                title: 'get_next_line',
+                symbol: '🍌',
+                link: 'https://github.com/khantorot/get_next_line',
+                description: 'Whether it`s a file, standard input, or even later a network connection, you`ll still need to read content line by line.'
+            },
+            {
+                title: 'init',
+                symbol: '🍋',
+                link: 'https://github.com/khantorot/init',
+                description: 'This project is an introduction to system and network administration.'
+            },
+            {
+                title: 'fillit',
+                symbol: '🍑',
+                link: 'https://github.com/khantorot/fillit',
+                description: 'Fillit is a project allowing you to discover and / or familiarize yourself with a recurring problem in programming: the search for an optimal solution among a very large number of possibilities, within a reasonable time. In the case of this project, it will be a question of arranging the Tetriminos between them and of determining the smallest possible edge which can accommodate them.'
+            },
+            {
+                title: 'ft_printf',
+                symbol: '🍓',
+                link: 'https://github.com/khantorot/ft_printf',
+                description: 'Tired of doing your postings by alternating ft_putstr and ft_putnbr? Are you not allowed to use printf? Recode yours! This will be an opportunity to discover a feature of C - variadic functions - and to practice fine management of display options. '
+            },
+            {
+                title: 'push_swap',
+                symbol: '🥑',
+                link: 'https://github.com/khantorot/push_swap',
+                description: 'This project asks you to sort data on a stack, with a limited set of instructions, in as few moves as possible.'
+            },
+            {
+                title: 'fdf',
+                symbol: '🥕',
+                link: 'https://github.com/khantorot/fdf',
+                description: 'To start your journey to the land of graphic programming, FdF offers you to represent the wire mesh of a terrain in 3D.'
+            },
+            {
+                title: 'fractol',
+                symbol: '🥝',
+                link: 'https://github.com/khantorot/fractol',
+                description: 'Discover 2D programming and the psychedelic universe of fractals thanks to this project to be carried out with the minilibX.'
+            },
+            {
+                title: 'rtv1',
+                symbol: '🥒',
+                link: 'https://github.com/khantorot/rtv1',
+                description: 'Discover the fundamentals of Raytracing: the creation of computer generated images. This first project will have you making simple scenes involving non-complex objects and lights.'
+            },
+            {
+                title: 'wolf3d',
+                symbol: '🍐',
+                link: 'https://github.com/khantorot/wolf3d',
+                description: 'Inspired by the famous eponymous game of the 90s which was the first First Person Shooter, this project allows you to approach the technique of ray-casting.'
+            },
+            {
+                title: 'piscine_php',
+                symbol: '🍎',
+                link: 'https://github.com/khantorot/piscine_php',
+                description: 'The PHP pool will allow you to access basic web technologies.'
+            },
+        ]
+    },
+]
